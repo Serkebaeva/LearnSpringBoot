c93f44896ff4cb4eefb69b395477d3112e4dd286
Serkebaeva <aizada.serkebaeva.lt@gmail.com>
2024-09-18 07:49:49 +0200

    Learn Spring Boot, Spring boot actuator & devtools

--- learn-spring-boot/src/main/java/com/in28minutes/learnspringboot/courses/controller/CourseController.java
+++ learn-spring-boot/src/main/java/com/in28minutes/learnspringboot/courses/controller/CourseController.java
@@ -14,6 +14,20 @@ public class CourseController {
 
 	@GetMapping("/courses")
     public List<Course> getAllCourses() {
-      return Arrays.asList(new Course(1, "Learn Microservices", "in28minutes"));
+      return Arrays.asList(new Course(1, "Learn Microservices", "in28minutes"), 
+    		  new Course(2, "Learn Full Stack with Angular and React", "in 28minutes"));            
   }
+	
+	// http://localhost:8080/courses/1
+	@GetMapping("/courses/1")
+    public Course getCourseDetails() {
+      return new Course(1, "Learn Java", "in28minutes");            
+  }
+	
+	// http://localhost:8080/courses/2
+	@GetMapping("/courses/2")
+    public Course getCourseInfo() {
+      return new Course(2, "Learn Python", "in28minutes");            
+  }
+	
 }
